52b550ad5fe82b1919d644d8620f590b46b779b7
Radu Tanasa <radu@geneguard.bio>
2023-04-23 10:20:43 +0300

    make func public

--- system.go
+++ system.go
@@ -1,6 +1,6 @@
 package dxgo
 
-func (c *DXClient) findDataObjects() {
+func (c *DXClient) FindDataObjects() {
 	data, err := c.retryableRequest("/system/findDataObjects", struct{}{})
 	if err != nil {
 		println(err)
