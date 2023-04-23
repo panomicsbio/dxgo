2162ad7ee63b393e85b0abfdc7098f24029b9c6b
Radu Tanasa <radu@geneguard.bio>
2023-04-23 10:28:04 +0300

    print error

--- system.go
+++ system.go
@@ -3,7 +3,7 @@ package dxgo
 func (c *DXClient) FindDataObjects() {
 	data, err := c.retryableRequest("/system/findDataObjects", struct{}{})
 	if err != nil {
-		println(err)
+		println(err.Error())
 		return
 	}
 	println("----------")
