50d07b835366f6fae914efdd34bd08e2937373ea
Robert Krajla <robert@panomics.bio>
2025-02-14 12:24:30 +0200

    Applet Get details

--- dxgo/applet.go
+++ dxgo/applet.go
@@ -29,3 +29,43 @@ func (c *DXClient) AppletRun(ctx context.Context, input AppletRunInput) (AppletR
 
 	return *output, nil
 }
+
+// AppletGetOutput represents the full specification of an applet
+type AppletGetOutput struct {
+	ID             string            `json:"id"`
+	Project        string            `json:"project"`
+	Class          string            `json:"class"`
+	Types          []string          `json:"types"`
+	Created        int64             `json:"created"`
+	State          string            `json:"state"`
+	Hidden         bool              `json:"hidden"`
+	Links          []string          `json:"links"`
+	Name           string            `json:"name"`
+	Folder         string            `json:"folder"`
+	Sponsored      bool              `json:"sponsored"`
+	Tags           []string          `json:"tags"`
+	Modified       int64             `json:"modified"`
+	CreatedBy      CreatedBy         `json:"createdBy"`
+	RunSpec        RunSpec           `json:"runSpec"`
+	DxAPI          string            `json:"dxapi"`
+	Access         Access            `json:"access"`
+	Title          string            `json:"title"`
+	Summary        string            `json:"summary"`
+	Description    string            `json:"description"`
+	DeveloperNotes string            `json:"developerNotes"`
+	IgnoreReuse    bool              `json:"ignoreReuse"`
+	InputSpec      []IOSpec          `json:"inputSpec,omitempty"`
+	OutputSpec     []IOSpec          `json:"outputSpec,omitempty"`
+	Properties     map[string]string `json:"properties,omitempty"`
+	Details        interface{}       `json:"details,omitempty"`
+}
+
+func (c *DXClient) AppletGet(ctx context.Context, appletID string) (AppletGetOutput, error) {
+	output := new(AppletGetOutput)
+	err := c.DoInto(ctx, fmt.Sprintf("/%s/get", appletID), nil, output)
+	if err != nil {
+		return AppletGetOutput{}, fmt.Errorf("getting applet: %w", err)
+	}
+
+	return *output, nil
+}
--- dxgo/dto.go
+++ dxgo/dto.go
@@ -38,8 +38,65 @@ type DXSortByOrdering string
 const Ascending DXSortByOrdering = "ascending"
 const Descending DXSortByOrdering = "descending"
 
-type SystemRequirementsValue struct {
-	InstanceType string `json:"instanceType"`
+type CreatedBy struct {
+	User       string `json:"user"`
+	Job        string `json:"job,omitempty"`
+	Executable string `json:"executable,omitempty"`
 }
 
-type SystemRequirements map[string]*SystemRequirementsValue
+// RunSpec represents the execution specification for an applet or app
+type RunSpec struct {
+	Code                   string             `json:"code"`
+	Interpreter            string             `json:"interpreter"`
+	Distribution           string             `json:"distribution,omitempty"`
+	Release                string             `json:"release,omitempty"`
+	Version                string             `json:"version,omitempty"`
+	RestartableEntryPoints bool               `json:"restartableEntryPoints,omitempty"`
+	SystemRequirements     SystemRequirements `json:"systemRequirements,omitempty"`
+	TimeoutPolicy          *TimeoutPolicy     `json:"timeoutPolicy,omitempty"`
+}
+
+// TimeoutPolicy represents the timeout configuration for an execution
+type TimeoutPolicy struct {
+	Hours   *int `json:"*,omitempty"`
+	Main    *int `json:"main,omitempty"`
+	Default *int `json:"default,omitempty"`
+}
+
+// IOSpec represents input/output specifications for an applet or app
+type IOSpec struct {
+	Name        string   `json:"name"`
+	Class       string   `json:"class"`
+	Optional    bool     `json:"optional,omitempty"`
+	Types       []string `json:"types,omitempty"`
+	Patterns    []string `json:"patterns,omitempty"`
+	Help        string   `json:"help,omitempty"`
+	Label       string   `json:"label,omitempty"`
+	Default     any      `json:"default,omitempty"`
+	Choices     []any    `json:"choices,omitempty"`
+	Group       string   `json:"group,omitempty"`
+	Suggestions []any    `json:"suggestions,omitempty"`
+}
+
+// Access represents access control settings for a resource
+type Access struct {
+	Network       []string          `json:"network"`
+	Project       string            `json:"project"`
+	AllProjects   string            `json:"allProjects"`
+	Developer     bool              `json:"developer"`
+	ProjectAccess string            `json:"projectAccess"`
+	Permissions   map[string]string `json:"permissions,omitempty"`
+}
+
+// SystemRequirements represents specific computational resource requirements
+type SystemRequirements struct {
+	InstanceType string                                  `json:"instanceType,omitempty"`
+	ClusterSpec  string                                  `json:"clusterSpec,omitempty"`
+	EntryPoints  map[string]SystemRequirementsEntryPoint `json:"entryPoints,omitempty"`
+}
+
+// SystemRequirementEntryPoint represents requirements for a specific entry point
+type SystemRequirementsEntryPoint struct {
+	InstanceType string `json:"instanceType,omitempty"`
+	ClusterSpec  string `json:"clusterSpec,omitempty"`
+}
